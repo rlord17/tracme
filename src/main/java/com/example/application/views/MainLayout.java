c5bce0aaf58bb6f540e0a2355b9324cbb4cd2b39
start.vaadin.com <no-reply@vaadin.com>
2023-09-10 11:22:55 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/MainLayout.java
@@ -0,0 +1,198 @@
+package com.example.application.views;
+
+import com.example.application.data.entity.User;
+import com.example.application.security.AuthenticatedUser;
+import com.example.application.views.about.AboutView;
+import com.example.application.views.addressform.AddressFormView;
+import com.example.application.views.cardlist.CardListView;
+import com.example.application.views.chat.ChatView;
+import com.example.application.views.checkoutform.CheckoutFormView;
+import com.example.application.views.collaborativemasterdetail.CollaborativeMasterDetailView;
+import com.example.application.views.creditcardform.CreditCardFormView;
+import com.example.application.views.empty.EmptyView;
+import com.example.application.views.gridwithfilters.GridwithFiltersView;
+import com.example.application.views.helloworld.HelloWorldView;
+import com.example.application.views.helloworld2.HelloWorld2View;
+import com.example.application.views.imagelist.ImageListView;
+import com.example.application.views.masterdetail.MasterDetailView;
+import com.example.application.views.personform.PersonFormView;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.avatar.Avatar;
+import com.vaadin.flow.component.contextmenu.MenuItem;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Footer;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Header;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.menubar.MenuBar;
+import com.vaadin.flow.component.orderedlayout.Scroller;
+import com.vaadin.flow.component.sidenav.SideNav;
+import com.vaadin.flow.component.sidenav.SideNavItem;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.server.StreamResource;
+import com.vaadin.flow.server.auth.AccessAnnotationChecker;
+import com.vaadin.flow.theme.lumo.LumoUtility;
+import java.io.ByteArrayInputStream;
+import java.util.Optional;
+import org.vaadin.lineawesome.LineAwesomeIcon;
+
+/**
+ * The main view is a top-level placeholder for other views.
+ */
+public class MainLayout extends AppLayout {
+
+    private H2 viewTitle;
+
+    private AuthenticatedUser authenticatedUser;
+    private AccessAnnotationChecker accessChecker;
+
+    public MainLayout(AuthenticatedUser authenticatedUser, AccessAnnotationChecker accessChecker) {
+        this.authenticatedUser = authenticatedUser;
+        this.accessChecker = accessChecker;
+
+        setPrimarySection(Section.DRAWER);
+        addDrawerContent();
+        addHeaderContent();
+    }
+
+    private void addHeaderContent() {
+        DrawerToggle toggle = new DrawerToggle();
+        toggle.setAriaLabel("Menu toggle");
+
+        viewTitle = new H2();
+        viewTitle.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.Margin.NONE);
+
+        addToNavbar(true, toggle, viewTitle);
+    }
+
+    private void addDrawerContent() {
+        H1 appName = new H1("My App");
+        appName.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.Margin.NONE);
+        Header header = new Header(appName);
+
+        Scroller scroller = new Scroller(createNavigation());
+
+        addToDrawer(header, scroller, createFooter());
+    }
+
+    private SideNav createNavigation() {
+        SideNav nav = new SideNav();
+
+        if (accessChecker.hasAccess(HelloWorldView.class)) {
+            nav.addItem(new SideNavItem("Hello World", HelloWorldView.class, LineAwesomeIcon.GLOBE_SOLID.create()));
+
+        }
+        if (accessChecker.hasAccess(AboutView.class)) {
+            nav.addItem(new SideNavItem("About", AboutView.class, LineAwesomeIcon.FILE.create()));
+
+        }
+        if (accessChecker.hasAccess(EmptyView.class)) {
+            nav.addItem(new SideNavItem("Empty", EmptyView.class, LineAwesomeIcon.FILE.create()));
+
+        }
+        if (accessChecker.hasAccess(HelloWorld2View.class)) {
+            nav.addItem(new SideNavItem("Hello World2", HelloWorld2View.class, LineAwesomeIcon.GLOBE_SOLID.create()));
+
+        }
+        if (accessChecker.hasAccess(CardListView.class)) {
+            nav.addItem(new SideNavItem("Card List", CardListView.class, LineAwesomeIcon.LIST_SOLID.create()));
+
+        }
+        if (accessChecker.hasAccess(MasterDetailView.class)) {
+            nav.addItem(
+                    new SideNavItem("Master-Detail", MasterDetailView.class, LineAwesomeIcon.COLUMNS_SOLID.create()));
+
+        }
+        if (accessChecker.hasAccess(CollaborativeMasterDetailView.class)) {
+            nav.addItem(new SideNavItem("Collaborative Master-Detail", CollaborativeMasterDetailView.class,
+                    LineAwesomeIcon.COLUMNS_SOLID.create()));
+
+        }
+        if (accessChecker.hasAccess(PersonFormView.class)) {
+            nav.addItem(new SideNavItem("Person Form", PersonFormView.class, LineAwesomeIcon.USER.create()));
+
+        }
+        if (accessChecker.hasAccess(AddressFormView.class)) {
+            nav.addItem(
+                    new SideNavItem("Address Form", AddressFormView.class, LineAwesomeIcon.MAP_MARKER_SOLID.create()));
+
+        }
+        if (accessChecker.hasAccess(CreditCardFormView.class)) {
+            nav.addItem(new SideNavItem("Credit Card Form", CreditCardFormView.class,
+                    LineAwesomeIcon.CREDIT_CARD.create()));
+
+        }
+        if (accessChecker.hasAccess(ChatView.class)) {
+            nav.addItem(new SideNavItem("Chat", ChatView.class, LineAwesomeIcon.COMMENTS.create()));
+
+        }
+        if (accessChecker.hasAccess(ImageListView.class)) {
+            nav.addItem(new SideNavItem("Image List", ImageListView.class, LineAwesomeIcon.TH_LIST_SOLID.create()));
+
+        }
+        if (accessChecker.hasAccess(CheckoutFormView.class)) {
+            nav.addItem(new SideNavItem("Checkout Form", CheckoutFormView.class, LineAwesomeIcon.CREDIT_CARD.create()));
+
+        }
+        if (accessChecker.hasAccess(GridwithFiltersView.class)) {
+            nav.addItem(new SideNavItem("Grid with Filters", GridwithFiltersView.class,
+                    LineAwesomeIcon.FILTER_SOLID.create()));
+
+        }
+
+        return nav;
+    }
+
+    private Footer createFooter() {
+        Footer layout = new Footer();
+
+        Optional<User> maybeUser = authenticatedUser.get();
+        if (maybeUser.isPresent()) {
+            User user = maybeUser.get();
+
+            Avatar avatar = new Avatar(user.getName());
+            StreamResource resource = new StreamResource("profile-pic",
+                    () -> new ByteArrayInputStream(user.getProfilePicture()));
+            avatar.setImageResource(resource);
+            avatar.setThemeName("xsmall");
+            avatar.getElement().setAttribute("tabindex", "-1");
+
+            MenuBar userMenu = new MenuBar();
+            userMenu.setThemeName("tertiary-inline contrast");
+
+            MenuItem userName = userMenu.addItem("");
+            Div div = new Div();
+            div.add(avatar);
+            div.add(user.getName());
+            div.add(new Icon("lumo", "dropdown"));
+            div.getElement().getStyle().set("display", "flex");
+            div.getElement().getStyle().set("align-items", "center");
+            div.getElement().getStyle().set("gap", "var(--lumo-space-s)");
+            userName.add(div);
+            userName.getSubMenu().addItem("Sign out", e -> {
+                authenticatedUser.logout();
+            });
+
+            layout.add(userMenu);
+        } else {
+            Anchor loginLink = new Anchor("login", "Sign in");
+            layout.add(loginLink);
+        }
+
+        return layout;
+    }
+
+    @Override
+    protected void afterNavigation() {
+        super.afterNavigation();
+        viewTitle.setText(getCurrentPageTitle());
+    }
+
+    private String getCurrentPageTitle() {
+        PageTitle title = getContent().getClass().getAnnotation(PageTitle.class);
+        return title == null ? "" : title.value();
+    }
+}
